b01a9749d49a97cbf26be837dc1b723235c947c1
NT <usedforfunny@gmail.com>
2022-09-16 17:22:25 +0200

    fetch data (addresses) from wikipedia, depending on your maker's position

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     
     @State private var locations = [Location]()
     
+    @State private var selectedPlace: Location?
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
@@ -26,6 +28,10 @@ struct ContentView: View {
                             .clipShape(Circle())
                         
                         Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        selectedPlace = location
                     }
                 }
             }
@@ -54,6 +60,13 @@ struct ContentView: View {
                     .font(.title)
                     .clipShape(Circle())
                     .padding(.trailing)
+                }
+            }
+        }
+        .sheet(item: $selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
                 }
             }
         }
